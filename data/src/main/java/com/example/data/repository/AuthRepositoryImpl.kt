a4014d4c8324312b3757815c50843bedb74248e3
Tohir Mirxomitov <mirxtohir@gmail.com>
2025-07-26 19:31:53 +0500

    FIX: delay

--- data/src/main/java/com/example/data/repository/AuthRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/AuthRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package com.example.data.repository
 
 import com.example.domain.repository.AuthRepository
+import kotlinx.coroutines.delay
 
 class AuthRepositoryImpl : AuthRepository {
     override suspend fun login(email: String, password: String): Result<Unit> {
+        delay(500)
         return Result.success(Unit)
     }
 }
